b8c655026bdae062b956e08b58995360abc6548a
ArturSamvelyan <yourcamelcase@gmail.com>
2019-10-19 12:40:04 +0300

    Bouncing Triangle

--- 2_coding_actions/main.cpp
+++ 2_coding_actions/main.cpp
@@ -214,8 +214,8 @@ int main(){
 
     glm::mat4 model = glm::mat4(1.0);  
     //std::cout << glm::to_string(model) << std::endl;
-    model = glm::rotate(model, curAngle*toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
     model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+    model = glm::rotate(model, curAngle*toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
 
     glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
